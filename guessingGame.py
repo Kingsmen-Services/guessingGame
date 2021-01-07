313f04a25a74af6ce45312530e3bbc0bf5c7a07c
NiggleFritz <77024218+NiggleFritz@users.noreply.github.com>
2021-01-07 12:09:55 -0700

    Added comments
    Just added comments to clarify code

--- guessingGame.py
+++ guessingGame.py
@@ -4,6 +4,7 @@ r = random.randint
 surrender = 4
 done = 7
 D = str(input("Select difficulty mortal. [easy/med/hard]"))
+#Sets Variable for max tries 'done' offering 'surrender' the random number and how many tries your on 'tries'
 if D == "easy":
     r = random.randint(1, 100)
     print("Figures. Choose a number between 1 and 100")
@@ -14,6 +15,7 @@ if D == "hard":
     r = random.randint(1, 1000000)
     print("Someone has a death wish. Choose a number between 1 and 1,000,000")
 G = int(input(""))
+#Allows user to set difficult and mocks them appropriatly
 while G != r:
     if G > r:
         print("Has aiming high ever worked for you.... really?")
@@ -23,6 +25,7 @@ while G != r:
         print("Maybe you should have higher aspirations in life.")
         G = int(input("Try again....."))
         tries = tries + 1
+        #Lets user know to guess higher or lower and adds to the 'tries' with every incorrect guess
     if tries == surrender:
         R = str(input("Are you ready to admit my superior intellect?"))
         if R == "no":
@@ -30,6 +33,7 @@ while G != r:
         elif R == "yes":
             print("This is why computers will take over the world!")
             break
+            #Allows user to surrender after 4 guesses
     if tries == done:
         if G == r:
             print("I would congratulate you, but even a broken clock is right twice a day.")
@@ -37,4 +41,4 @@ while G != r:
         else:
             print("This is why computers will take over the world")
             exit()
-       
+            #This section deal with the end of the game, either you win and it mocks you, you lose and it mocks you
